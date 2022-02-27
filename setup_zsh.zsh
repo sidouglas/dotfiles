724b365317baec9719576405c23308606957a8ae
Simon Douglas <1107946+sidouglas@users.noreply.github.com>
2022-02-27 14:35:02 +1100

    add .zsh files

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -1,11 +1,8 @@
 #!/usr/bin/env zsh
 
 # https://ohmyz.sh/#install
-#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-
-# Customisation to zsh-oh-my-gosh, personal aliases, functions etc.
-echo 'source ~/.zsh_custom' >> ~/.zshrc
 
 # Vendor specific or private
 echo 'ZSH_SHARED="~/Google Drive/My Drive/shared/.zsh_shared"' >> ~/.zshrc
---
+++ zshenv
@@ -0,0 +1,5 @@
+function exists() { command -v $1 > /dev/null 2>$1 }
+
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
